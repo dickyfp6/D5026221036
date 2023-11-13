7d9ca19d40b4d3080b76455029d295a7c1162076
dickyfp6 <147455930+dickyfp6@users.noreply.github.com>
2023-11-13 13:40:16 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('halo', function () {
+    return "Halo, Selamat datang di tutorial laravel www.malasngoding.com";
+});
+
+Route::get('halo2', function () {
+    return "Halo, Selamat datang";
+});
+
+Route::get('/blog', function ($id) {
+    return view('blog');
+});
+
+Route::get('/week1', function () {
+    return view('week1');
+});
