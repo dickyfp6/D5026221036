480786e56442136219a30ac0fc9a63646f4e3c8d
dickyfp6 <147455930+dickyfp6@users.noreply.github.com>
2023-11-13 15:34:34 +0700

    Pertemuan 13 Nov 23

---
+++ app/Http/Controllers/DosenController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DosenController extends Controller
+{
+    //
+
+
+public function index(){
+    $a=3;
+    $b=4;
+    $c=$a*$b;
+    return "<h1>Hasil perkalian ".(string)$c."</h1>";
+}
+public function showBlog(){
+    $nama="Dicky";
+    $alamat="Gebang, Surabaya";
+    $umur=19;
+    return view('blog', ['nama'== $nama,'alamt'== $alamat,'umur'== $umur]);
+}
+public function shownama($nama){
+    return "Anda telah mengisi " .$nama;
+}
+
+ public function formulir(){
+    return view('formulir');
+}
+
+public function proses(Request $request){
+    $nama = $request->input('nama');
+     $alamat = $request->input('alamat');
+    return "Nama :  ".$nama.", Alamat : ".$alamat;
+}
+
+}
+
--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,20 @@ Route::get('halo2', function () {
     return "Halo, Selamat datang";
 });
 
-Route::get('/blog', function ($id) {
+Route::get('/blog2', function ($id) {
     return view('blog');
 });
 
 Route::get('/week1', function () {
     return view('week1');
 });
+
+Route::get('/pegawai/{nama}', 'App\Http\Controllers\DosenController@shownama');
+
+Route::get('/formulir', 'App\Http\Controllers\DosenController@formulir');
+
+Route::post('/formulir/proses', 'App\Http\Controllers\DosenController@proses');
+
+Route::get('/blog', 'App\Http\Controllers\BlogController@home');
+Route::get('/blog/tentang', 'App\Http\Controllers\BlogController@tentang');
+Route::get('/blog/kontak', 'App\Http\Controllers\BlogController@kontak');
